07394412553d1ad9f735ed315b63474db72f524a
leowright <leow91863@gmail.com>
2025-06-16 08:46:13 -0600

    initial

---
+++ client/src/Pages/Users/CreateEmployee.jsx
@@ -0,0 +1,262 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createEmployee } from "../../redux/action/user";
+import { useNavigate } from "react-router-dom";
+import Topbar from "./Topbar";
+import {
+  Divider,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  DialogActions,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { PiNotepad, PiXLight } from "react-icons/pi";
+import { CFormSelect } from "@coreui/react";
+import { pakistanCities } from "../../constant";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+const CreateUser = ({ open, setOpen, scroll }) => {
+  //////////////////////////////////////// VARIABLES /////////////////////////////////////
+  const { isFetching } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const initialEmployeeState = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    phone: "",
+    email: "",
+  }
+
+  //////////////////////////////////////// STATES /////////////////////////////////////
+  const [employeeData, setEmployeeData] = useState(initialEmployeeState);
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    phone: '',
+    email: ''
+  });
+
+  //////////////////////////////////////// USE EFFECTS /////////////////////////////////////
+
+  //////////////////////////////////////// FUNCTIONS /////////////////////////////////////
+  const validateField = (field, value) => {
+    let error = '';
+    switch (field) {
+      case 'firstName':
+        if (!value.trim()) error = 'First name is required';
+        break;
+      case 'lastName':
+        if (!value.trim()) error = 'Last name is required';
+        break;
+      case 'username':
+        if (!value.trim()) error = 'Username is required';
+        break;
+      case 'password':
+        if (!value.trim()) error = 'Password is required';
+        else if (value.length < 6) error = 'Password must be at least 6 characters';
+        break;
+      case 'phone':
+        if (!value.trim()) error = 'Phone number is required';
+        else if (!/^\d{10,11}$/.test(value)) error = 'Please enter a valid phone number';
+        break;
+      case 'email':
+        if (value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) error = 'Please enter a valid email address';
+        break;
+      default:
+        break;
+    }
+    return error;
+  };
+
+  const handleChange = (field, value) => {
+    setEmployeeData((prevFilters) => ({ ...prevFilters, [field]: value }));
+    setErrors(prev => ({ ...prev, [field]: validateField(field, value) }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Validate all fields
+    const newErrors = {
+      firstName: validateField('firstName', employeeData.firstName),
+      lastName: validateField('lastName', employeeData.lastName),
+      username: validateField('username', employeeData.username),
+      password: validateField('password', employeeData.password),
+      phone: validateField('phone', employeeData.phone),
+      email: validateField('email', employeeData.email)
+    };
+    
+    setErrors(newErrors);
+    
+    // Check if there are any errors
+    const hasErrors = Object.values(newErrors).some(error => error !== '');
+    
+    if (!hasErrors) {
+      dispatch(createEmployee(employeeData, setOpen));
+      setEmployeeData(initialEmployeeState);
+      setErrors({
+        firstName: '',
+        lastName: '',
+        username: '',
+        password: '',
+        phone: '',
+        email: ''
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setEmployeeData(initialEmployeeState);
+    setErrors({
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: '',
+      phone: '',
+      email: ''
+    });
+  };
+
+  return (
+    <div>
+      <Dialog
+        scroll={scroll}
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        fullWidth="sm"
+        maxWidth="sm"
+        aria-describedby="alert-dialog-slide-description">
+        <DialogTitle className="flex items-center justify-between">
+          <div className="text-sky-400 font-primary">Add New Employee</div>
+          <div className="cursor-pointer" onClick={handleClose}>
+            <PiXLight className="text-[25px]" />
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <div className="flex flex-col gap-2 p-3 text-gray-500 font-primary">
+            <div className="text-xl flex justify-start items-center gap-2 font-normal">
+              <PiNotepad size={23} />
+              <span>Employee Details</span>
+            </div>
+            <Divider />
+            <table className="mt-4">
+              <tr>
+                <td className="pb-4 text-lg">First Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={employeeData.firstName}
+                    onChange={(e) => handleChange('firstName', e.target.value)}
+                    error={!!errors.firstName}
+                    helperText={errors.firstName}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">Last Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={employeeData.lastName}
+                    onChange={(e) => handleChange('lastName', e.target.value)}
+                    error={!!errors.lastName}
+                    helperText={errors.lastName}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">User Name </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    value={employeeData.username}
+                    onChange={(e) => handleChange('username', e.target.value)}
+                    error={!!errors.username}
+                    helperText={errors.username}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="pb-4 text-lg">Email </td>
+                <td className="pb-4">
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Optional"
+                    value={employeeData.email}
+                    onChange={(e) => handleChange('email', e.target.value)}
+                    error={!!errors.email}
+                    helperText={errors.email}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="flex items-start pt-2 text-lg">Password </td>
+                <td className="pb-4">
+                  <TextField
+                    type="password"
+                    value={employeeData.password}
+                    onChange={(e) => handleChange("password", e.target.value)}
+                    size="small"
+                    fullWidth
+                    error={!!errors.password}
+                    helperText={errors.password}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td className="flex items-start pt-2 text-lg">Phone </td>
+                <td className="pb-4">
+                  <TextField
+                    type="number"
+                    size="small"
+                    value={employeeData.phone}
+                    onChange={(e) => handleChange("phone", e.target.value)}
+                    fullWidth
+                    error={!!errors.phone}
+                    helperText={errors.phone}
+                  />
+                </td>
+              </tr>
+            </table>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <button
+            onClick={handleClose}
+            variant="contained"
+            type="reset"
+            className="bg-[#d7d7d7] px-4 py-2 rounded-lg text-gray-500 mt-4 hover:text-white hover:bg-[#6c757d] border-[2px] border-[#efeeee] hover:border-[#d7d7d7] font-thin transition-all">
+            Cancel
+          </button>
+          <button
+            onClick={handleSubmit}
+            variant="contained"
+            className="bg-primary-red px-4 py-2 rounded-lg text-white mt-4 hover:bg-red-400 font-thin">
+            {isFetching ? 'Submitting...' : 'Submit'}
+          </button>
+        </DialogActions>
+      </Dialog>
+    </div>
+
+  );
+};
+
+export default CreateUser;
